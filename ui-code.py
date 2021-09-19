6bd42268c21963ad936a635bdd0b4cab9a97aade
Digionix <jonathanvieri1321@gmail.com>
2021-09-20 05:20:34 +1000

    Added Thread implementation and fixed custom functionality
    Added the Thread implementation into the UI code and fixed the custom temperature functionality which was not working correctly

--- ui-code.py
+++ ui-code.py
@@ -1,13 +1,45 @@
 # Importing necessary modules
 from tkinter import *
 from time import sleep
+import threading
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import board
 
 #Set UI temperature and moisture
-global custom_temperature = 20
-global custom_moisture = 0
+global custom_temperature 
+global custom_moisture 
+custom_temperature = 20
+custom_moisture = 0
+
+#Set up threads
+class uiThread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        window.mainloop()
+
+class readingThread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        while True:
+            actual_moisture = GPIO.input(MOISTURE_INPUT)
+            print(actual_moisture)
+            humidity, actual_temperature = Adafruit_DHT.read_retry(DHT_SENSOR, TEMP_INPUT)
+            print(actual_temperature)
+            if actual_temperature < custom_temperature - 1:
+                print("Plant is too cold")
+            elif actual_temperature > custom_temperature + 1:
+                print("Plant is too hot")
+            if actual_moisture == 0:
+                print("Plant is dry. Turning on hose")
+            elif actual_moisture == 1:
+                print("Plant is damp enough")
+            sleep(1)
+
+UI = uiThread()
+readings = readingThread()
 
 # set up sensor input
 GPIO.setmode(GPIO.BCM)
@@ -16,20 +48,7 @@ MOISTURE_INPUT = 18
 DHT_SENSOR = Adafruit_DHT.DHT22
 GPIO.setup(MOISTURE_INPUT, GPIO.IN)
 GPIO.setup(TEMP_INPUT, GPIO.IN)
-while True:
-    actual_moisture = GPIO.input(MOISTURE_INPUT)
-    print(actual_moisture)
-    humidity, actual_temperature = Adafruit_DHT.read_retry(DHT_SENSOR, TEMP_INPUT)
-    print(actual_temperature)
-    if actual_temperature < custom_temperature - 1:
-        print("Plant is too cold")
-    elif actual_temperature > custom_temperature + 1:
-        print("Plant is too hot")
-    if actual_moisture == 0:
-        print("Plant is dry. Turning on hose")
-    elif actual_moisture == 1:
-        print("Plant is damp enough")
-    sleep(1)
+
 
 # =======================================================
 # Edit these variables to modify the UI elements
@@ -70,6 +89,7 @@ buttonFontSize = 10
 # Function that will be called when pressing the buttons
 # Button for increasing temperature
 def increaseTemperature():
+    global custom_temperature
     custom_temperature += 1
     result = str(custom_temperature) + "°C"
     lblTempVal.config(text = result)
@@ -77,6 +97,7 @@ def increaseTemperature():
 
 # Button for lowering temperature
 def lowerTemperature():
+    global custom_temperature
     custom_temperature -= 1
     result = str(custom_temperature) + "°C"
     lblTempVal.config(text = result)
@@ -114,7 +135,7 @@ lblMoistHeader = Label(
 
 # Label for temperature value
 lblTempVal = Label(
-    text=custom_temperature + "°C",
+    text=str(custom_temperature) + "°C",
     font=(vFont, vFontSize),
     fg='black',
     bg='white',
@@ -124,7 +145,7 @@ lblTempVal = Label(
 
 # Label for moisture level value
 lblMoistVal = Label(
-    text=custom_moisture + "%",
+    text=str(custom_moisture) + "%",
     font=(vFont, vFontSize),
     fg='black',
     bg='white',
